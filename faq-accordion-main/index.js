cf40bf76ca4ee3677281b8971cff56e7b184f491
Carlos Pizarro <122272599+IncorrigibleSpirit@users.noreply.github.com>
2024-07-31 23:07:31 +0900

    Delete faq-accordion-main directory
    Delete folder faq-accordion

--- faq-accordion-main/index.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".faq_heading").forEach((heading) => {
-  heading.addEventListener("click", () => {
-    const faqItem = heading.parentElement;
-    faqItem.classList.toggle("active");
-  });
-});
