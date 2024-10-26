ae44d633e7a938290288dafb3509ec1f915a2b58
Carlos Pizarro <122272599+IncorrigibleSpirit@users.noreply.github.com>
2024-10-26 19:19:04 +0900

    Update index.js
    added comment/explanation

--- faq-accordion-main/index.js
+++ faq-accordion-main/index.js
@@ -1,6 +1,6 @@
 document.querySelectorAll(".faq_heading").forEach((heading) => {
   heading.addEventListener("click", () => {
-    const faqItem = heading.parentElement;
+    const faqItem = heading.parentElement; // //Asi se representa el valor del contenedor padre, a fin de obtenerlo
     faqItem.classList.toggle("active");
   });
 });
