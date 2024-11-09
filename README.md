1d56208abd58947bf7a578910e725a96b5b0a380
carlos Pizarro <contacto.carlospizarro@gmail.com>
2024-11-09 22:12:29 +0900

    Update project structure and add new files
    - Replaced styles.css with index.scss
    - Added footer to index.html
    - Added README.md

---
+++ README.md
@@ -0,0 +1,41 @@
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+The following exercise is part of the web design course: Learn modern HTML5, CSS3, and web design by Jonas Schmedtmann.
+
+### The challenge
+
+Users should be able to see the carousel on desktop and tablet devices.
+
+### Screenshot
+
+![Screenshot](./fotos/Screenshot%20-%20Carousel.png)
+
+### Links
+
+- Solution URL: [GitHub]()
+- Live Site URL: [Carousel]()
+
+## My process
+
+### Built with
+
+- Semantic HTML5
+- CSS
+- Javascript
+- CSS Flexbox
+- Sass
+
+## Author
+
+- Website - [Carlos Pizarro](https://github.com/IncorrigibleSpirit)
+- Frontend Mentor - [@IncorrigibleSpirit](https://www.frontendmentor.io/profile/IncorrigibleSpirit)
--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // VARIABLES
-const carouselContainer = document.querySelector(".carousel-slides");
+const carouselSlides = document.querySelector(".carousel-slides");
 const prevBtn = document.getElementById("prevBtn");
 const nextBtn = document.getElementById("nextBtn");
 const dots = document.querySelectorAll(".carousel-dot");
 
 // VALORES INICIALES
-let currentSlide = 0;
+let currentSlide = 0; //Incrementa +1 con botón (nextBtn) /  Decrementa -1 con prevBtn).
 const totalSlides = document.querySelectorAll(".carousel-item").length;
 
 // FUNCIONES
 
 // Función encargada de mover la slide del carrusel en el eje x para mostrar la diapositiva actual.
 const updateSlidePosition = () => {
-  carouselContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
+  carouselSlides.style.transform = `translateX(-${currentSlide * 100}%)`;
   updateDots();
 };
 
-// Función que actualiza los puntos. Utiliza forEach para recorrer cada punto y crear un índice.
+// Función que actualiza los indicadores. Utiliza forEach para recorrer cada punto y crear un índice.
 // index: índice del elemento que se esta iterando.
 // currentSlide: La diapositiva actual
 const updateDots = () => {
@@ -35,11 +35,16 @@ nextBtn.addEventListener("click", () => {
   currentSlide = (currentSlide + 1) % totalSlides;
   updateSlidePosition();
 });
+/*% totalSlides: El operador módulo (%) se utiliza para asegurarse de que cuando lleguemos al final del carrusel, volvamos al inicio. Si totalSlides es, por ejemplo, 5, cuando currentSlide + 1 sea igual a 5 (que sería la sexta diapositiva), al aplicar el MODULO RESIDUO DE DIVISION (5 % 5), el valor será 0, lo que significa que volvemos a la primera diapositiva.
+ */
 
 prevBtn.addEventListener("click", () => {
   currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
   updateSlidePosition();
 });
+/*currentSlide - 1: Decrementa el número de la diapositiva actual. Es decir, si estamos en la diapositiva 3, ahora pasaremos a la 2.
++ totalSlides: Esto asegura que el valor de currentSlide nunca sea negativo.
+Por ejemplo, si estamos en la primera diapositiva (currentSlide = 0) y restamos 1, el valor sería -1. Pero al sumar el total de diapositivas (totalSlides), obtenemos:((0-1) + 5) % 5 =  MODULO RESIDUO DE DIVISION 0 */
 
 //Añade un evento para cambiar a la diapositiva actual y actualizar la posición y los puntos.
 dots.forEach((dot, index) => {
